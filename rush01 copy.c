07a65b58917cd89275caf3f07405e88cb1075bd4
Zhao Colen <zcolen@ox-k3.21-school.ru>
2020-03-07 23:42:10 +0300

    final

--- rush01 copy.c
+++ rush01 copy.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
@@ -114,73 +116,6 @@ char **fill_arr(char **str)
     return (arr_sc);
 }
 
-int    see_answer_add(char *c, int *count, int k)
-{
-    int z;
-
-
-    if (c - '0' > 0)
-    {
-        count++;
-        z = k;
-    }
-    return (z);
-}
-
-void    see_answer(char **arr, char ***arr3d)
-{
-    int i;
-    int j;
-    int k;
-    int count;
-    int z;
-
-    i = 0;
-    while (i < 9)
-    {
-        j = 0;
-        while (j < 9)
-        {
-            count = 0;
-            k = 0;
-            while (k < 9)
-            {
-                z = see_answer_add(&arr3d[i][j][k], &count, k);
-                k++;
-            }
-            if (count == 1)
-                arr[i][j] = arr3d[i][j][z];
-            j++;
-        }
-        i++;
-    }
-}
-
-void    del_sc(char **arr_sc, char ***arr3d)
-{
-    int i;
-    int j;
-    int k;
-
-    i = 0;
-    while (i < 9)
-    {
-        j = 0;
-        while (j < 9)
-        {
-            k = 0;
-            while (k < 0)
-            {
-                if (arr_sc[which_sector(i, j)][k] != '0')
-                    arr3d[i][j][k - 1] = '0';
-                k++;
-            }
-            j++;
-        }
-        i++;
-    }
-}
-
 void    display_3d(char ***arr)
 {
     int     i;
@@ -261,7 +196,7 @@ int     check_del_hv(char el, int i, int j, char **arr)
     result = 0;
     while (x < 9)
     {
-        if (arr[i][x] == el || arr[x][i] == el)
+        if (arr[i][x] == el || arr[x][j] == el)
             result = 1;
         x++;
     }
@@ -292,17 +227,186 @@ void    del_hv(char ***arr3d, char **arr)
     }
 }
 
+int     check_del_sc(char el, int i, int j, char **arr)
+{
+    int x;
+    int result;
+    x = 0;
+    result = 0;
+    while (x < 9)
+    {
+        if (arr[which_sector(i, j)][x] == el)
+            result = 1;
+        x++;
+    }
+    return (result);
+}
+
+void    del_sc(char ***arr3d, char **arr, char **arr_sc) //удаление элементов по секторам
+{
+    int i;
+    int j;
+    int k;
+    i = 0;
+    while(i < 9)
+    {
+        j = 0;
+        while (j < 9)
+        {
+            k = 0;
+            while (k < 9)
+            {
+                if (arr[i][j] == '0' && check_del_sc(arr3d[i][j][k], i, j, arr_sc) == 1)
+                    arr3d[i][j][k] = '0';
+                k++;
+            }
+            j++;
+        }
+        i++;
+    }
+}
+
+void    see_answer(char **arr, char ***arr3d)
+{
+    int i;
+    int j;
+    int k;
+    int count;
+    char z;
+
+    i = 0;
+    while (i < 9)
+    {
+        j = 0;
+        while (j < 9)
+        {
+            count = 0;
+            k = 0;
+            if (arr[i][j] == '0')
+            {
+                while (k < 9)
+                {
+                    if (arr3d[i][j][k] != '0')
+                    {
+                        count++;
+                        z = arr3d[i][j][k];
+                    }
+                    k++;
+                }
+                if (count == 1)
+                    arr[i][j] = z;
+            }
+            j++;
+        }
+        i++;
+    }
+}
+
+int     check_for_zero(char **arr)
+{
+    int i;
+    int j;
+    int count;
+
+    i = 0;
+    count = 0;
+    while (i < 9)
+    {
+        j = 0;
+        while (j < 9)
+        {
+            if (arr[i][j] == '0')
+                count++;
+            j++;
+        }
+        i++;
+    }
+    return (count);
+}
+
+void    final_check_add(char **arr, char ***arr3d, int i, int j)
+{
+    int k;
+    int z;
+    int count;
+
+    k = 0;
+    while (k < 9)
+    {
+        if (arr3d[i][j][k] != '0')
+        {
+            z = 0;
+            count = 0;
+            while (z < 9)
+            {
+                if (arr3d[i][z][k] != '0')
+                    count++;
+                z++;
+            }
+            if (count == 1)
+                arr[i][j] = arr3d[i][j][k];
+        }
+        k++;
+    }
+}
+void    final_check(char **arr, char ***arr3d)
+{
+    int i;
+    int j;
+    int k;
+    int count;
+    char z;
+
+    i = 0;
+    while (i < 9)
+    {
+        j = 0;
+        while (j < 9)
+        {
+            count = 0;
+            k = 0;
+            if (arr[i][j] == '0')
+            {
+                while (k < 9)
+                {
+                    if (arr3d[i][j][k] != '0')
+                        count++;
+                    k++;
+                }
+                if (count > 1)
+                {
+                    final_check_add(arr, arr3d, i, j);
+                }
+            }
+            j++;
+        }
+        i++;
+    }
+}
+
+
 int main(int argc, char **argv)
 {
     char **arr;
     char **arr_sc;
     char ***arr3d;
+    int k;
+    int i;
 
+    k = 0;
+    i = 0;
     arr = fill_arr(argv);
     arr_sc = trans_to_arr_sc(arr);
     arr3d = fill_arr3d();
     sort_3d_from_arr(arr3d, arr);
-    del_hv(arr3d, arr);
-    display_3d(arr3d);
+    while (check_for_zero(arr) > 0 && check_for_zero(arr) != k) {
+        k = check_for_zero(arr);
+        del_hv(arr3d, arr);
+        del_sc(arr3d, arr, arr_sc);
+        arr_sc = trans_to_arr_sc(arr);
+        see_answer(arr, arr3d);
+        if (check_for_zero(arr) == k)
+            final_check(arr, arr3d);
+    }
+    display_2d(arr);
 }
-
